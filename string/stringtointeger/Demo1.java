5021b806506559df27cb3c49c32570f9d96f5e46
Bhar-2003 <bharathjaya5016@gmail.com>
2025-04-26 06:32:33 +0530

    javaprograms

---
+++ string/stringtointeger/Demo1.java
@@ -0,0 +1,29 @@
+package string.stringtointeger;
+
+public class Demo1 {
+    // Time Complexity: O(n)
+    // Auxiliary Space: O(1)
+    public static int myAtoi(String s){
+        int sign=1;
+        int result=0;
+        int idx=0;
+        while (idx<s.length() && s.charAt(idx)==' ') {
+            idx++;
+        }
+        if(idx<s.length() && (s.charAt(idx)=='-'|| s.charAt(idx)=='+' )){
+            if (s.charAt(idx++)=='-')
+                sign=-1;
+        }
+        while (idx<s.length() && s.charAt(idx)>='0' && s.charAt(idx)<='9'){
+            if(result>Integer.MAX_VALUE/10 || (result==Integer.MAX_VALUE/10 && s.charAt(idx) -'0'>7 )){
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result=10*result+(s.charAt(idx++)-'0');
+        }
+        return result*sign;
+    }
+    public static void main(String[] args) {
+        String s = "  -0012g4";
+        System.out.println(myAtoi(s));
+    }
+}
